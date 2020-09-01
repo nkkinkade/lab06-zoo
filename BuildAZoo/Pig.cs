57903cf61ff058eb6079b192b89e139b2a23e69c
Nathan Kinkade <nkkinkade@gmail.com>
2020-09-01 09:51:34 -0500

    add pig

--- BuildAZoo/Pig.cs
+++ BuildAZoo/Pig.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace BuildAZoo
 {
-    class Pig
+    public abstract class Pig : FarmAnimal
     {
+        // Inherited Properties
+        public override int Legs => 4;
+
+        // Inherited Methods
+        public override bool HasFur
+        {
+            get { return false; }
+        }
+        public override string Movement()
+        {
+            return "walk";
+        }
+
+        public override string Sound()
+        {
+            return "oink";
+        }
+
+        public override string Eat()
+        {
+            return "slop";
+        }
     }
 }
